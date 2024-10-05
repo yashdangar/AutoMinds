0d47d0cebfcf6fda0a451987877793fc4525eddf
Dev <devitaliya22@gmail.com>
2024-10-06 01:04:58 +0530

    URL clean up

--- src/app/(pages)/connections/_components/Card.tsx
+++ src/app/(pages)/connections/_components/Card.tsx
@@ -38,7 +38,7 @@ const ConnectionCard = ({
     } else if (type === "Github") {
       console.log(process.env.GITHUB_CLIENT_ID);
       
-      window.location.href = `https://github.com/login/oauth/authorize?client_id=Ov23liAkBOVl2yyHb3My&scope=repo%20user%20gist%20notifications%20read%3Aorg&response_type=code&redirect_uri=https%3A%2F%2Fauto-minds-six.vercel.app%2Fapi%2Fgithub%2Fcallback&state=${generateRandomString()}`;
+      window.location.href = `https://github.com/login/oauth/authorize?client_id=Ov23liF8APtIGZxmwZtz&scope=repo%20user%20gist%20notifications%20read%3Aorg&response_type=code&redirect_uri=https%3A%2F%2Fauto-minds-six.vercel.app%2Fapi%2Fgithub%2Fcallback&state=${generateRandomString()}`;
     }
   };
   
